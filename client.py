8954239ab9bfe8b4c8c9c14db36fcfbbdcb42173
Elias Lengrube <eliaslengrube@gmail.com>
2021-01-28 19:35:49 -0300

    first commit

---
+++ client.py
@@ -0,0 +1,29 @@
+#Variables
+host = 'localhost'
+port = 8050
+#Se importa el módulo
+import socket
+
+#Creación de un objeto socket (lado cliente)
+obj = socket.socket()
+
+#Conexión con el servidor. Parametros: IP (puede ser del tipo 192.168.1.1 o localhost), Puerto
+obj.connect((host, port))
+print("Conectado al servidor")
+
+#Creamos un bucle para retener la conexion
+while True:
+    #Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
+    mens = input("Mensaje desde Cliente a Servidor >> ")
+    #Con el método send, enviamos el mensaje
+    obj.send(mens.encode('utf8'))
+    #obj.send(mens)
+    recibido = obj.recv(1024)
+    print(recibido)
+
+#Cerramos la instancia del objeto servidor
+obj.close()
+
+#Imprimimos la palabra Adios para cuando se cierre la conexion
+print("Conexión cerrada")
+
